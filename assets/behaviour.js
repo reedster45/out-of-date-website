3aa643d1a989d48721764b0c4ec129cbe2eaa20f
Reed Tyhuis <tyhuis34@gmail.com>
2021-07-14 15:49:52 -0400

    major redesign

--- assets/behaviour.js
+++ assets/behaviour.js
@@ -6,20 +6,7 @@ document.getElementById('ab').addEventListener('click', function(){setActiveOnCl
 document.getElementById('po').addEventListener('click', function(){setActiveOnClick('po')});
 document.getElementById('co').addEventListener('click', function(){setActiveOnClick('co')});
 document.addEventListener('scroll', setActiveOnScroll);
-document.addEventListener('scroll', stickyTop);
 
-
-function stickyTop(){
-  var nav = document.getElementById('navbar');
-  var about = document.getElementById('about');
-  var bar = about.getBoundingClientRect();
-  
-  if(bar.top <= -100){
-    nav.classList.add('sticky-top');
-  } else {
-    nav.classList.remove('sticky-top');
-  }
-}
 
 function autoScroll(){
   window.scrollTo(0, document.getElementById('about').offsetTop);
